71e26a7ecd0562aad0fa753e2bd123e3c2e8e115
Dinis Machado <dinisma@gmail.com>
2024-04-09 20:34:09 +0000

    Add Log in/out views and is_owner field

---
+++ profiles/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return obj.owner == request.user
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id',
+            'owner',
+            'created_at',
+            'updated_at',
+            'name',
+            'content',
+            'image',
+            'is_owner'
+        ]
